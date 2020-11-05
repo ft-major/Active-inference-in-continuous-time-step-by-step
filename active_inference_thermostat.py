deea387eb115949310b20eff038523da09c44e70
Tone <tone@pop-os.localdomain>
2020-11-05 17:49:49 +0100

    updated .ipynb and added matlab original code

--- active_inference_thermostat.py
+++ active_inference_thermostat.py
@@ -92,6 +92,8 @@
 # x'(t+dt) = x'(t) - k_{a} \left[ \frac{ \partial F }{ \partial x' } \right] = x'(t) - k_{a} \left[ \frac{ \partial F }{ \partial s' } \frac{ \partial s' }{ dx' } \right] = x'(t) - k_{a} \left[ \frac{ \varepsilon_{s'} }{ \Sigma_{s'} } (-T_0\frac{ 2x }{ (x^2+1)^2 }) \right]
 # $$
 
+#%%
+
 #%% md
 # # Code
 
@@ -105,20 +107,11 @@ import matplotlib.pyplot as plt
 
 
 #%% md
-# ## Simulation parameters
-#%%
-simT=100                            # Simulation Time
-dt=0.005                            # Time step lenght
-print(100/0.005)
-steps=range(int(simT/dt))                # Time step number
-action=True                         # Variable to enable action
-
-#%% md
 # ## Classes
 #%%
+# Generative process class
 class GenProc:
-    # Generative process class
-    def __init__(self, rng, x, x1):
+    def __init__(self, rng, x):
 
         # Generative process parameters
         self.SigmaGP_s = 0.1                        # Generative process s variance
@@ -129,10 +122,10 @@ class GenProc:
         self.x = x                                  # Two dimensional array storing agent position and velocity
 
     def T(self):                                    # System temperature
-        return self.T0/(self.x[0]^2+1)
+        return self.T0/(self.x[0]**2+1)
 
     def T1(self):                                   # System temperature "velocity"
-        Tx = -2*self.T0*self.x[0]/((self.x[0]^2+1)^2)
+        Tx = -2*self.T0*self.x[0]/((self.x[0]**1+1)**2)
         return Tx*self.x[1]
 
     def genS(self):                                 # Function that create agent's sensory input (two dimensional array)
@@ -141,12 +134,12 @@ class GenProc:
         s[1] = self.SigmaGP_s1*self.rng.randn() + self.T1()
         return s
 
-    def step(self, dt):                             # Step of generative process dynamic
+    def dynamic(self, dt):                             # Step of generative process dynamic
         self.x[0] += dt*self.x[1]
 
+# Generative model class
 class GenMod:
-    # Generative model class
-    def __init__(self, rng, dt):
+    def __init__(self, rng, dt, a, mu, s):
 
         self.s = s                                  # Two dimensional array storing sensory input s and s'
         self.a = a                                  # Action variable
@@ -154,13 +147,13 @@ class GenMod:
 
         # Generative model parameters
         self.Td = 4                                 # Desired temperature
-        self.actionTime=0                           # Variable that enable action only after a fixed number of steps
-        self.Sigma_s = 0.1                          # Generative model s variance (in this case we're assuming the agent knows gp variace)
-        self.Sigma_s1 = 0.1                         # Generative model s' variance (in this case we're assuming the agent knows gp variace)
-        self.Sigma_mu = 0.1                         # Generative model $\mu$ variance
-        self.Sigma_mu1 = 0.1                        # Generative model $\mu'$ variance
+        self.actionTime= 25                          # Variable that enable action only after a fixed amount of time
+        self.Sigma_s = 0.1                            # Generative model s variance (in this case we're assuming the agent knows gp variace)
+        self.Sigma_s1 = 0.1                           # Generative model s' variance (in this case we're assuming the agent knows gp variace)
+        self.Sigma_mu = 0.1                           # Generative model $\mu$ variance
+        self.Sigma_mu1 = 0.1                          # Generative model $\mu'$ variance
         self.k_mu = 0.1*dt                          # Gradient descent inference parameter
-        self.k_a = 0.1*dt                           # Gradient descent action parameter
+        self.k_a = 0.01*dt                           # Gradient descent action parameter
 
     def f(self):                                    # f(mu) dynamics generative model
         return -self.mu[0]+self.Td
@@ -168,23 +161,83 @@ class GenMod:
     def VFE(self):                                  # Variational Free Energy
         epsilon_s = self.s[0] - self.mu[0]
         epsilon_s1 = self.s[1] - self.mu[1]
-        epsilon_mu = self.mu[1] - f()
+        epsilon_mu = self.mu[1] - self.f()
         epsilon_mu1 = self.mu[2] + self.mu[1]
-        return 1/2*( espilon_s^2/self.Sigma_s + espilon_s1^2/self.Sigma_s1 + espilon_mu^2/self.Sigma_mu + espilon_mu1^2/self.Sigma_mu1 )
+        return 1/2*( espilon_s**2/self.Sigma_s + espilon_s1**2/self.Sigma_s1 + espilon_mu**2/self.Sigma_mu + espilon_mu1**2/self.Sigma_mu1 )
 
     def update(self, dt, step, x, T0):
         epsilon_s = self.s[0] - self.mu[0]
         epsilon_s1 = self.s[1] - self.mu[1]
-        epsilon_mu = self.mu[1] - f()
+        epsilon_mu = self.mu[1] - self.f()
         epsilon_mu1 = self.mu[2] + self.mu[1]
         self.mu[0] += dt*self.mu[1] - self.k_mu*( - epsilon_s/self.Sigma_s + epsilon_mu/self.Sigma_mu )
         self.mu[1] += dt*self.mu[2] - self.k_mu*( - epsilon_s1/self.Sigma_s1 + epsilon_mu/self.Sigma_mu + epsilon_mu1/self.Sigma_mu1)
         self.mu[2] += - self.k_mu*( epsilon_mu1/self.Sigma_mu1 )
-        if self.actionTime<=step:
-            Tx = -2*T0*.x/((.x^2+1)^2)
+        if self.actionTime<=step*dt:
+            Tx = +1 #-2*T0*x/((x**2+1)**2)
             self.a += -self.k_a*Tx*epsilon_s1/self.Sigma_s1
 
+# Plotter class
+class Plotter:
+
+    def __init__(self, len_data):
+        self.fig = plt.figure(figsize=(12, 8))
+        self.ax = self.fig.add_subplot(111)
+        #self.line, = self.ax.plot(0, 0, lw=3, c="black")
+        self.eq, = self.ax.plot(0, 0)
+        self.x = np.zeros(len_data)
+        self.y = np.zeros(len_data)
+        #self.eq, = self.ax.plot(self.x, self.y)
+
+    def update_data(self, i, x, y):
+        self.x[i] = x
+        self.y[i] = y
+
+    def update_plot(self, i):
+
+        self.eq.set_data(self.x[:i+1], self.y[i+1])
+        #self.ax.set_xlim(self.x[0], self.x[-1])
+        #self.ax.set_ylim(self.y[0], self.y[-1])
+        self.fig.canvas.draw()
+
+
+#%% md
+# ## Simulation
 #%%
-g=GenMod(rng=0,x=100, x1=0)
-g.x=1
-print(g.T)
+if __name__ == "__main__":
+    simT = 100                                      # Simulation Time
+    dt = 0.005                                      # Time step lenght
+    steps = int(simT/dt)                            # Time step number
+    action = True                                   # Variable to enable action
+    rng = np.random.RandomState(42)
+    gp = GenProc( rng, x=np.zeros(2) )              # Initialising generative process
+    gp.x[0] = 2                                     # Agent starting point
+    gm = GenMod( rng, dt, s=np.zeros(2), mu=np.zeros(3), a=0 )
+                                                    # Initialasing generative model
+
+    TvsSteps = Plotter(steps)                   # Initialasing Temperature dynamic plot
+    #TvsSteps.update(0, )
+    T = np.array([gp.T()])
+    pos = np.array([gp.x[0]])
+    act = np.array([gm.a])
+    for i in range(1,steps):
+        #print(i),
+        gp.x[1] = gm.a                              # Agent's action (set its own velocity)
+        gp.dynamic(dt)                                # The environment evolves following the dynamic given by x and x'
+        gm.s = gp.genS()                            # The generative process creates noisy sensory input that are percevied by the generative model
+        gm.update(dt, i, gp.x[0], gp.T0)            # The generative model update internal states mu, mu' and mu'' computing the dynamic,
+                                                    # and taking the VFE gradient descent (every step of the gradient descent is simultaneous with the internal
+                                                    # model updating step, that is an assumpion)
+        TvsSteps.get_data(i*dt, gp.T())                  # Updating Temperature dynamic plot
+        T = np.append(T,np.array(gp.T()))
+        pos = np.append(pos,gp.x[0])
+        act = np.append(act,gm.a)
+    #TvsSteps.plt.show()
+
+#%%
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(111)
+temp, = ax.plot(np.arange(steps)*dt, T)
+position, = ax.plot(np.arange(steps)*dt, pos)
+action, = ax.plot(np.arange(steps)*dt, act)
+#fig.canvas.draw()
